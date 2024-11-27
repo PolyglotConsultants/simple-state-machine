15fde695f5337aa9f0366dfb931362c67accb268
Gopakumar Nair <gopa@PolyglotConsultants.com>
2024-11-27 16:34:45 +0530

    Fix tests failing in CI but works locally.

--- __tests__/state-holder.spec.ts
+++ __tests__/state-holder.spec.ts
@@ -1,19 +1,23 @@
 import { StateHolder } from '../src/state-holder';
 import { StateKey } from '../src/state-key';
+import {Subscription} from "rxjs";
 
 describe('StateHolder', () => {
   let stateHolder: StateHolder;
+  let subs:Subscription;
   const CounterKey = new StateKey<number>('counter');
 
   beforeEach(() => {
     stateHolder = StateHolder.getInstance();
+    if(subs)
+      subs.unsubscribe();
   });
 
   it('should store and retrieve state', (done) => {
     stateHolder.put(CounterKey, 10);
     const state$ = stateHolder.observe(CounterKey);
 
-    state$?.subscribe((value) => {
+    subs = state$?.subscribe((value) => {
       expect(value).toBe(10);
       done();
     });
